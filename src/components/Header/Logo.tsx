6ccb839461d1cc356b17a8077d0d3e65b5c20196
BentAllenDesign <64994378+BentAllenDesign@users.noreply.github.com>
2024-03-13 17:43:14 -0500

    fix: refactoring Logo to be a pure return

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -6,16 +6,14 @@ interface LogoProps {
   label: string;
 }
 
-export const Logo: React.FC<LogoProps> = ({ label }) => {
-  return (
-    <div className="flex justify-center items-center select-none group">
-      {/* The main composer app for whatever reason has these as separate buttons, so I did too */}
-      <Link to='/' className="pr-1 h-full flex items-center">
-        <img src={logoImage} alt="Logo" className="h-8 w-8 group-hover:drop-shadow-lg" />
-      </Link>
-      <Link to="/" className="text-4xl font-display h-full text-gray-800 tracking-tighter no-underline group-hover:drop-shadow-md group-hover:text-black">
-        {label}
-      </Link>
-    </div>
-  );
-};
+export const Logo: React.FC<LogoProps> = ({ label }) => (
+  <div className="flex justify-center items-center select-none group">
+    {/* The main composer app for whatever reason has these as separate buttons, so I did too */}
+    <Link to='/' className="pr-1 h-full flex items-center">
+      <img src={logoImage} alt="Logo" className="h-8 w-8 group-hover:drop-shadow-lg" />
+    </Link>
+    <Link to="/" className="text-4xl font-display h-full text-gray-800 tracking-tighter no-underline group-hover:drop-shadow-md group-hover:text-black">
+      {label}
+    </Link>
+  </div>
+);
